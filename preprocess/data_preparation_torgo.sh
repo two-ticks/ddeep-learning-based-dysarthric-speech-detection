8459b316d67ad5497ff4f7e5afb1406d36858c7d
two-ticks <adityasiddheshwar.adisid@gmail.com>
2024-04-19 02:50:23 +0530

    Update data_preparation_torgo.sh script for TORGO dataset

--- preprocess/data_preparation_torgo.sh
+++ preprocess/data_preparation_torgo.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-# Define the base URL and target directory
-BASE_URL="http://www.cs.toronto.edu/~complingweb/data/TORGO/"
 TARGET_DIR="Torgo"
 
-# Define the file names
-FILES=("F.tar.bz2" "FC.tar.bz2" "M.tar.bz2" "MC.tar.bz2")
-
 # Check if zip files are already present
-missing_files=false
-for file in "${FILES[@]}"; do
-    if [ ! -f "${TARGET_DIR}/${file}" ]; then
-        missing_files=true
-        break
-    fi
-done
-
-# If any zip file is not present, download them
-if $missing_files; then
-    for file in "${FILES[@]}"; do
-        wget "${BASE_URL}${file}" -P "${TARGET_DIR}"
-    done
+if [ ! -f "${TARGET_DIR}/F.tar.bz2" ] || [ ! -f "${TARGET_DIR}/FC.tar.bz2" ] || [ ! -f "${TARGET_DIR}/M.tar.bz2" ] || [ ! -f "${TARGET_DIR}/MC.tar.bz2" ]; then
+    # If any zip file is not present, download them
+    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/F.tar.bz2 -P Torgo/
+    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/FC.tar.bz2 -P Torgo/
+    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/M.tar.bz2 -P Torgo/
+    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/MC.tar.bz2 -P Torgo/
 fi
 
 # Extract files
