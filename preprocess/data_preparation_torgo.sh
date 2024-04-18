e909cf45866d515aa6bd1130ca7d4bdc4e2c1011
two-ticks <adityasiddheshwar.adisid@gmail.com>
2024-04-19 02:52:33 +0530

    Update data_preparation_torgo.sh script for TORGO dataset

--- preprocess/data_preparation_torgo.sh
+++ preprocess/data_preparation_torgo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-TARGET_DIR="Torgo"
+TARGET_DIR="/scratch/Torgo"
 
 # Check if zip files are already present
 if [ ! -f "${TARGET_DIR}/F.tar.bz2" ] || [ ! -f "${TARGET_DIR}/FC.tar.bz2" ] || [ ! -f "${TARGET_DIR}/M.tar.bz2" ] || [ ! -f "${TARGET_DIR}/MC.tar.bz2" ]; then
     # If any zip file is not present, download them
-    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/F.tar.bz2 -P Torgo/
-    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/FC.tar.bz2 -P Torgo/
-    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/M.tar.bz2 -P Torgo/
-    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/MC.tar.bz2 -P Torgo/
+    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/F.tar.bz2 -P ${TARGET_DIR}/
+    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/FC.tar.bz2 -P ${TARGET_DIR}/
+    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/M.tar.bz2 -P ${TARGET_DIR}/
+    wget http://www.cs.toronto.edu/~complingweb/data/TORGO/MC.tar.bz2 -P ${TARGET_DIR}/
 fi
 
 # Extract files
