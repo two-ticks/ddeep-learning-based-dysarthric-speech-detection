c28bc96c26752d52c6c4fc6ba5adc3db779a5ea1
two-ticks <adityasiddheshwar.adisid@gmail.com>
2024-04-19 02:26:54 +0530

    Refactor data preparation script for TORGO dataset

--- preprocess/data_preparation_torgo.sh
+++ preprocess/data_preparation_torgo.sh
@@ -1,9 +1,35 @@
-wget http://www.cs.toronto.edu/~complingweb/data/TORGO/F.tar.bz2 -P Torgo/
-wget http://www.cs.toronto.edu/~complingweb/data/TORGO/FC.tar.bz2 -P Torgo/
-wget http://www.cs.toronto.edu/~complingweb/data/TORGO/M.tar.bz2 -P Torgo/
-wget http://www.cs.toronto.edu/~complingweb/data/TORGO/MC.tar.bz2 -P Torgo/
+#!/bin/bash
 
-tar -xvf Torgo/F.tar.bz2 -C Torgo/dysarthria
-tar -xvf Torgo/FC.tar.bz2 -C Torgo/non_dysarthria
-tar -xvf Torgo/M.tar.bz2 -C Torgo/dysarthria
-tar -xvf Torgo/MC.tar.bz2 -C Torgo/non_dysarthria
+# Define the URLs and target directory
+urls=(
+    "http://www.cs.toronto.edu/~complingweb/data/TORGO/F.tar.bz2"
+    "http://www.cs.toronto.edu/~complingweb/data/TORGO/FC.tar.bz2"
+    "http://www.cs.toronto.edu/~complingweb/data/TORGO/M.tar.bz2"
+    "http://www.cs.toronto.edu/~complingweb/data/TORGO/MC.tar.bz2"
+)
+
+target_dir="/scratch/TORGO"
+
+# Check if zip files are already present
+all_present=true
+for url in "${urls[@]}"; do
+    if [ ! -f "${target_dir}/${url##*/}" ]; then
+        all_present=false
+        break
+    fi
+done
+
+# If any zip file is not present, download them
+if ! $all_present; then
+    for url in "${urls[@]}"; do
+        if [ ! -f "${target_dir}/${url##*/}" ]; then
+            wget "$url" -P "$target_dir"
+        fi
+    done
+fi
+
+# Extract files
+tar -xvf "${target_dir}/F.tar.bz2" -C "${target_dir}/dysarthria"
+tar -xvf "${target_dir}/FC.tar.bz2" -C "${target_dir}/non_dysarthria"
+tar -xvf "${target_dir}/M.tar.bz2" -C "${target_dir}/dysarthria"
+tar -xvf "${target_dir}/MC.tar.bz2" -C "${target_dir}/non_dysarthria"
